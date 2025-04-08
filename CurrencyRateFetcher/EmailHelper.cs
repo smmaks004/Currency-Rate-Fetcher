7ef273b23a7d1466b0662598f35d30a5dcd662bc
smmaks004 <42142486+smmaks004@users.noreply.github.com>
2025-04-08 17:11:38 +0300

    Removed unused using directives

--- CurrencyRateFetcher/EmailHelper.cs
+++ CurrencyRateFetcher/EmailHelper.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Mail;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
 using static CurrencyRateFetcher.SettingsHelper;
 
 namespace CurrencyRateFetcher
